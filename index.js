3796f00ae9f26e5bc0c05b4a7ffe8ca05b2abc8a
Zahra Khan <zarssskhan@gmail.com>
2017-09-08 05:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musician, instrument) {
   var musiciansAndInstruments = [];
   for (var i = 0; i < musician.length; i++) {
-    musiciansAndInstruments.push(`${musician[i]} plays ${instrument[i]}`)
+    musiciansAndInstruments.push(`${musician[i]} plays ${instrument[i]}.`);
   }
+  return musiciansAndInstruments;
 }
